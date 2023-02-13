0107fb5f76fa80eb0b8490e0a4f4423a58912da2
Witcher200 <igor.h8k@gmail.com>
2023-02-13 05:04:11 +0300

    ctFinal_proget

--- Final_project/src/test/java/Stage3/My_posts/ASC/Login/Login_User1Test.java
+++ Final_project/src/test/java/Stage3/My_posts/ASC/Login/Login_User1Test.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 ;
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.containsString;
 
 public class Login_User1Test extends AbstractPageTest{
 
@@ -18,16 +19,50 @@ public class Login_User1Test extends AbstractPageTest{
 
 	  @Test
 	  @Order(1)
-	  void LoginUser1Test() {
+	  void LoginUser_Ok() {
 			given()
 				.log()
 				.all()
-				.queryParam("username", "28022023")
-				.queryParam("password", "2be9e97498")
-				.expect()
-				.when()
-				.post(getLoginURL())
+				.params("username","28022023")
+				.params("password","2be9e97498")
+				.post("https://test-stand.gb.ru/gateway/login")
 				.then()
-				.statusCode(400);
+				.assertThat()
+				.body(containsString("token"))
+				.statusCode(200)
+				.log()
+				.all();
+	  }
+
+	  @Test
+	  @Order(2)
+	  void LoginUser_Wrong_creds() {
+			given()
+				.log()
+				.all()
+				.params("username","dfsfsdf")
+				.params("password","sdffsdfs")
+				.post("https://test-stand.gb.ru/gateway/login")
+				.then()
+				.assertThat()
+				.body(containsString("Invalid credentials"))
+				.statusCode(401)
+				.log()
+				.all();
+	  }
+
+	  @Test
+	  @Order(3)
+	  void LoginUser_Wrong_params() {
+			given()
+				.log()
+				.all()
+				.params("username","dfsfsdf")
+				.post("https://test-stand.gb.ru/gateway/login")
+				.then()
+				.assertThat()
+				.statusCode(400)
+				.log()
+				.all();
 	  }
 }
